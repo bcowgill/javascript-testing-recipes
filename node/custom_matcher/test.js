39487a1246e3b0c4cf1acc08323d5cee76fa5578
Brent S.A. Cowgill <zardoz@infoserve.net>
2017-07-24 20:48:55 +0100

    node/custom_matcher cleaned with (JS.Test) and JS || require

--- node/custom_matcher/test.js
+++ node/custom_matcher/test.js
@@ -1,21 +1,23 @@
-var JS = require("jstest")
+var JS = JS || require("jstest")
 
 var personNamed = function(name) {
-  return {
-    equals: function(value) {
-      return (typeof value === "object") && (value.name === name)
-    },
-    toString: function() {
-      return "Person named " + name
-    }
-  }
+	return {
+		equals: function(value) {
+			return (typeof value === "object") && (value.name === name)
+		},
+		toString: function() {
+			return "Person named " + name
+		}
+	}
 }
 
-JS.Test.describe("Person", function() { with(this) {
-  it("has a name", function() { with(this) {
-    assertEqual( personNamed("Bond"), {name: "Bond"} )
-  }})
-}})
+with (JS.Test) {
+	describe("Person", function() { with(this) {
+		it("has a name", function() { with(this) {
+			assertEqual( personNamed("Bond"), {name: "Bond"} )
+		}})
+	}})
 
-JS.Test.autorun()
+	autorun()
+}
 
