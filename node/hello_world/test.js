6cab2cd59f0c386f8e9208f82888ea5cfe488edd
Brent S.A. Cowgill <zardoz@infoserve.net>
2017-07-24 20:57:40 +0100

    */hello_world tests pass, skip, fail test example
    skip doesn't really work like mocha would

--- node/hello_world/test.js
+++ node/hello_world/test.js
@@ -1,11 +1,17 @@
 // test plan works in node and included in test.html from a browser
 var JS = JS || require("jstest")
 
-JS.Test.describe("Hello, world!", function() { with(this) {
-  it("runs a test", function() { with(this) {
-    assertEqual( "Hello, world!", ["Hello", "world!"].join(", ") )
-  }})
-}})
+with (JS.Test) {
+	describe("Hello, world!", function() { with(this) {
+		it("runs a test", function() { with(this) {
+			assertEqual( "Hello, world!", ["Hello", "world!"].join(", ") )
+		}})
+		it("skips a test")
+		it("fails a test", function() { with(this) {
+			assertEqual( "Hello, world!", ["Hello", "world!"].join(",  ") )
+		}})
+	}})
 
-JS.Test.autorun()
+	autorun()
+}
 
