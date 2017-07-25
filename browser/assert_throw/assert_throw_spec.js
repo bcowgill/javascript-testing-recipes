12ad464a6a21331a62eb6806d96e8d2519721112
Brent S.A. Cowgill <zardoz@infoserve.net>
2017-07-25 12:27:54 +0100

    assert_throw test detects phantomjs for error message test

--- browser/assert_throw/assert_throw_spec.js
+++ browser/assert_throw/assert_throw_spec.js
@@ -1,4 +1,45 @@
 with (JS.Test) {
+	var memoize = function (call) {
+		var memory
+		return function () {
+			return memory === void 0 ? (memory = call()) : memory
+		}
+	}
+
+	// http://engineering.shapesecurity.com/2015/01/detecting-phantomjs-based-visitors.html
+	var isPhantomJS = memoize(function () {
+		var error
+		var isPhantom = false
+		// console.error("isPhantomJS");
+		try {
+			null[0]()
+		}
+		catch (exception) {
+			// console.error("isPhantomJS catch ", exception, typeof exception.stack);
+			error = exception
+		}
+		if (error.stack.indexOf('phantomjs') > -1) {
+			isPhantom = 'phantom stack'
+		}
+		else if (window && (window.callPhantom || window._phantom)) {
+			isPhantom = 'phantom globals'
+		}
+		else if (navigator && (!(navigator.plugins instanceof PluginArray) || navigator.plugins.length == 0)) {
+			isPhantom = 'phantom plugins'
+		}
+		else if (!Function.prototype.bind) {
+			isPhantom = 'phantom bind1'
+		}
+		else if (Function.prototype.bind.toString().replace(/bind/g, 'Error') != Error.toString()) {
+			isPhantom = 'phantom bind2'
+		}
+		else if (Function.prototype.toString.toString().replace(/toString/g, 'Error') != Error.toString()) {
+			isPhantom = 'phantom bind3'
+		}
+		// console.log("isPhantomJS ", isPhantom)
+		return !!isPhantom
+	})
+
 	function throwMe () { "spline".reticulate() }
 
 	function wrap (fnThrow) {
@@ -20,8 +61,8 @@ with (JS.Test) {
 		it("examines the error message thrown", function () { with (this) {
 			var chromeErrorMessage = 'TypeError: "spline".reticulate is not a function'
 			var phantomErrorMessage = "TypeError: undefined is not a function (evaluating '\"spline\".reticulate()')"
-			//assertSame(wrap(throwMe), phantomErrorMessage)
-			assertSame(wrap(throwMe), chromeErrorMessage)
+			var expect = isPhantomJS() ? phantomErrorMessage : chromeErrorMessage;
+			assertSame(wrap(throwMe), expect)
 		}})
 		it("in case function does not throw", function () { with (this) {
 			assertSame(wrap(function () {}), "nothing thrown")
