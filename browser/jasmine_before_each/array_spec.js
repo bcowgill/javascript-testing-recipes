f6bc657fb1f0622564982032a96609cbe91352d0
Brent S.A. Cowgill <zardoz@infoserve.net>
2017-07-25 16:18:32 +0100

    number tests ran on all the array_spec files

--- browser/jasmine_before_each/array_spec.js
+++ browser/jasmine_before_each/array_spec.js
@@ -1,19 +1,19 @@
-describe("Array", function() {
+describe("Array [-array_spec:0-]", function() {
   var array
 
   beforeEach(function() {
     array = ["this", "wooden", "idea"]
   })
 
-  it("has a length", function() {
+  it("has a length [-array_spec:1-]", function() {
     expect(array.length).toBe(3)
   })
 
-  it("returns the value at an index", function() {
+  it("returns the value at an index [-array_spec:2-]", function() {
     expect(array[1]).toBe("wooden")
   })
 
-  it("returns the index of a value", function() {
+  it("returns the index of a value [-array_spec:3-]", function() {
     expect(array.indexOf("idea")).toBe(2)
   })
 })
--- node/array_scoped_tests/array_spec.js
+++ node/array_scoped_tests/array_spec.js
@@ -1,18 +1,18 @@
 with (JS.Test) {
-	describe("Array [-array_spec:0-]", function () { with (this) {
+	describe("Array [-array_spec:4-]", function () { with (this) {
 		before(function () { with (this) {
 			this.array = ["this", "wooden", "idea"]
 		}})
 
-		it("has a length [-array_spec:1-]", function () { with (this) {
+		it("has a length [-array_spec:5-]", function () { with (this) {
 			assertEqual( 3, array.length )
 		}})
 
-		it("returns the value at an index [-array_spec:2-]", function () { with (this) {
+		it("returns the value at an index [-array_spec:6-]", function () { with (this) {
 			assertEqual( "wooden", array[1] )
 		}})
 
-		it("returns the index of a value [-array_spec:3-]", function () { with (this) {
+		it("returns the index of a value [-array_spec:7-]", function () { with (this) {
 			assertEqual( 2, array.indexOf("idea") )
 		}})
 	}})
