e7e38f6c7b5c8d0cea47672808ce62e906afc7b7
Brent S.A. Cowgill <zardoz@infoserve.net>
2017-07-25 17:21:22 +0100

    browser/hello_mocks tab/with JS.Test cleanup

--- browser/hello_mocks/debug_mock_spec.js
+++ browser/hello_mocks/debug_mock_spec.js
@@ -1,7 +1,9 @@
-JS.Test.describe("debug() with mocks", function() { with(this) {
-  it("writes the message to the console with a timestamp", function() { with(this) {
-    expect(console, "info").given(match(/^[0-9]{13} hello$/))
-    debug("hello")
-  }})
-}})
+with (JS.Test) {
+	describe("debug() with mocks", function () { with (this) {
+		it("writes the message to the console with a timestamp", function () { with (this) {
+			expect(console, "info").given(match(/^[0-9]{13} hello$/))
+			debug("hello")
+		}})
+	}})
+}
 
--- browser/hello_mocks/debug_spy_spec.js
+++ browser/hello_mocks/debug_spy_spec.js
@@ -1,15 +1,17 @@
-JS.Test.describe("debug() with spies", function() { with(this) {
-  before(function() { with(this) {
-    sinon.spy(console, "info")
-  }})
+with (JS.Test) {
+	describe("debug() with spies", function () { with (this) {
+		before(function () { with (this) {
+			sinon.spy(console, "info")
+		}})
 
-  after(function() { with(this) {
-    console.info.restore()
-  }})
+		after(function () { with (this) {
+			console.info.restore()
+		}})
 
-  it("writes the message to the console with a timestamp", function() { with(this) {
-    debug("hello")
-    sinon.assert.calledWithMatch(console.info, /^[0-9]{13} hello$/)
-  }})
-}})
+		it("writes the message to the console with a timestamp", function () { with (this) {
+			debug("hello")
+			sinon.assert.calledWithMatch(console.info, /^[0-9]{13} hello$/)
+		}})
+	}})
+}
 
