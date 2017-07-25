0247c06f7cc4a45f407ce8f12cdb012c12d26553
Brent S.A. Cowgill <zardoz@infoserve.net>
2017-07-25 16:14:18 +0100

    node/array_scoped_tests tab/with JS.Test and numbering cleanup

--- node/array_scoped_tests/array_spec.js
+++ node/array_scoped_tests/array_spec.js
@@ -1,18 +1,20 @@
-JS.Test.describe("Array", function() { with(this) {
-  before(function() { with(this) {
-    this.array = ["this", "wooden", "idea"]
-  }})
+with (JS.Test) {
+	describe("Array [-array_spec:0-]", function () { with (this) {
+		before(function () { with (this) {
+			this.array = ["this", "wooden", "idea"]
+		}})
 
-  it("has a length", function() { with(this) {
-    assertEqual( 3, array.length )
-  }})
+		it("has a length [-array_spec:1-]", function () { with (this) {
+			assertEqual( 3, array.length )
+		}})
 
-  it("returns the value at an index", function() { with(this) {
-    assertEqual( "wooden", array[1] )
-  }})
+		it("returns the value at an index [-array_spec:2-]", function () { with (this) {
+			assertEqual( "wooden", array[1] )
+		}})
 
-  it("returns the index of a value", function() { with(this) {
-    assertEqual( 2, array.indexOf("idea") )
-  }})
-}})
+		it("returns the index of a value [-array_spec:3-]", function () { with (this) {
+			assertEqual( 2, array.indexOf("idea") )
+		}})
+	}})
+}
 
--- node/array_tests/array_spec.js
+++ node/array_tests/array_spec.js
@@ -1,16 +1,16 @@
 with (JS.Test) {
-	describe("Array [-array_spec:0-]", function () { with (this) {
-		it("has a length [-array_spec:1-]", function () { with (this) {
+	describe("Array [-array_spec:4-]", function () { with (this) {
+		it("has a length [-array_spec:5-]", function () { with (this) {
 			assertEqual( 0, [].length )
 			assertEqual( 3, [1, 2, 3].length )
 		}})
 
-		it("returns the value at an index [-array_spec:2-]", function () { with (this) {
+		it("returns the value at an index [-array_spec:6-]", function () { with (this) {
 			assertEqual( undefined, [][0] )
 			assertEqual( "b", ["a", "b", "c"][1] )
 		}})
 
-		it("returns the index of a value [-array_spec:3-]", function () { with (this) {
+		it("returns the index of a value [-array_spec:7-]", function () { with (this) {
 			assertEqual( -1, [].indexOf("thing") )
 			assertEqual( 2, ["this", "wooden", "idea"].indexOf("idea") )
 		}})
