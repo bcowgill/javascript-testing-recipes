8fd8f28bc507a25af8b3182ab633a74f0b1cb685
Brent S.A. Cowgill <zardoz@infoserve.net>
2017-07-26 16:58:12 +0100

    browser/hello_async tested test which takes too long to complete

--- browser/hello_async/counter.js
+++ browser/hello_async/counter.js
@@ -4,11 +4,12 @@ var Counter = function() {
 
 Counter.prototype.inc = function(n, callback) {
   var self = this
+  var DELAY = 10 //60000
   setTimeout(function() {
     // throw new Error("inc ooooooooooooooooops")
     self.count += n
     callback()
-  }, 10)
+  }, DELAY)
   // throw new Error("inc dddaaaawwwwggg")
 }
 
