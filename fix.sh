c18d56765985ae54d6ae2d50305fec636f1cc265
Brent S.A. Cowgill <zardoz@infoserve.net>
2017-07-27 09:42:30 +0100

    vendor/util and html_fixture tab/function () cleanup

--- fix.sh
+++ fix.sh
@@ -3,13 +3,13 @@
 FILE=$1
 if [ ! -z $FILE ]; then
 	perl -i.bak -pne '
-		s{\A}{\t}xmsg;
+		#s{\A}{\t}xmsg;
 		s{(function|with)\(}{$1 (}xmsg;
 		#s{JS \s+ = \s+ require}{JS = JS || require}xmsg;
-		s{\tJS\.Test\.describe}{with (JS.Test) \{\n\tdescribe}xmsg;
-		s{JS\.Test\.autorun\(\)}{autorun()\n\}}xmsg;
-		s{describe ( .+ with \s* \(this\) \s* \{ )}{describe$1 addSkip(this)}xmsg;
-		s[(\t}}\))][$1\n}]xmsg;
+		#s{\tJS\.Test\.describe}{with (JS.Test) \{\n\tdescribe}xmsg;
+		#s{JS\.Test\.autorun\(\)}{autorun()\n\}}xmsg;
+		#s{describe ( .+ with \s* \(this\) \s* \{ )}{describe$1 addSkip(this)}xmsg;
+		#s[(\t}}\))][$1\n}]xmsg;
 	' $FILE
 
 	INDENT=2 fix-spaces.sh $FILE
--- vendor/html_fixture.js
+++ vendor/html_fixture.js
@@ -1,19 +1,19 @@
 var HtmlFixture = {
-  fixture: function(html) {
-    this.before(function() {
-      var fixture = $("#fixture")
+	fixture: function (html) {
+		this.before(function () {
+			var fixture = $("#fixture")
 
-      if (fixture.length === 0) {
-        $("body").prepend('<div id="fixture"></div>')
-        fixture = $("#fixture")
-      }
-      this.fixture = fixture
-      this.fixture.html(html)
-    })
+			if (fixture.length === 0) {
+				$("body").prepend('<div id="fixture"></div>')
+				fixture = $("#fixture")
+			}
+			this.fixture = fixture
+			this.fixture.html(html)
+		})
 
-    this.after(function() {
-      this.fixture.empty()
-    })
-  }
+		this.after(function () {
+			this.fixture.empty()
+		})
+	}
 }
 
