9f1a92e128505efec808ea0d22843f8a572e1404
Brent S.A. Cowgill <zardoz@infoserve.net>
2017-07-29 20:11:14 +0100

    browser/ajax todo_service comparing CRUD/REST/Service/SQL and showing how to debug ajax

--- browser/ajax/todo_service.js
+++ browser/ajax/todo_service.js
@@ -1,3 +1,26 @@
+/*
+
+CRUD    REST       Service      SQL
+create  POST       save(no id)  INSERT INTO table (fields) VALUES (values)
+read    GET        load(id)     SELECT fields FROM table WHERE condition ...
+update  PUT/PATCH  save(w/id)   UPDATE table SET field=value WHERE condition
+delete  DELETE     remove(id)   DELETE FROM table WHERE condition
+
+	debugging ajax calls in the browser, use a log function
+	log = function (msg) { return function (resp) { console.log(msg, JSON.stringify(resp)) } }
+	> $.ajax({type: "GET", url: "/todos/1", success: log("ITEM"), error: log("ERROR")})
+	ITEM {"id":1,"title":"Mow the lawn"}
+	> $.ajax({type: "GET", url: "/todos/2", success: log("ITEM"), error: log("ERROR")})
+	ERROR {"readyState":4,"responseText":"{\"error\":\"Item not found\"}", ...
+	       "responseJSON":{"error":"Item not found"},"status":404,"statusText":"Not Found"}
+
+	debugging ajax with promises
+	> response = $.ajax({type: "PUT", url: "/todos/1", data: {title: "Mow the lawn"}})
+	> response.then(log("ITEM"), log("ERROR"))
+	ITEM {"id":1,"title":"Mow the lawn"}
+
+*/
+
 var TodoService = function() {}
 _.extend(TodoService.prototype, Backbone.Events)
 
@@ -26,12 +49,11 @@ TodoService.prototype.save = function(item, callback) {
       callback(new Error(error.responseJSON.error))
     })
   } else {
+    // $.post has no error handling support
     $.post("/todos/", item, function(response) {
       item.id = response.id
       self.trigger("create", item)
       callback(null, item)
-    }, function(error) {
-      callback(new Error(error.responseJSON.error))
     })
   }
 }
