23fe8335ca42989aca4b3660df5fe54d66fa9c61
Brent S.A. Cowgill <zardoz@infoserve.net>
2017-07-30 13:33:19 +0100

    browser/routing added test page for using backbone history.pushState instead of location.hash

---
+++ browser/routing/backbone_router_pushstate_spec.js
@@ -0,0 +1,21 @@
+with (JS.Test) {
+	describe("Router [-backbone_router_spec:0-]", function () { with (this) { addSkip(this)
+		before(function () { with (this) {
+			this.initialLocation = location.pathname
+		}})
+
+		after(function () { with (this) {
+			Backbone.history.navigate(initialLocation)
+		}})
+
+		it("routes / to timeline.html [-backbone_router_spec:1-]", function () { with (this) {
+			expect("render").given("timeline.html")
+			Backbone.history.navigate("/home", {trigger: true})
+		}})
+
+		it("routes /users/jack to profile.html with username=jack [-backbone_router_spec:2-]", function () { with (this) {
+			expect("render").given("profile.html", {username: "jack"})
+			Backbone.history.navigate("/users/jack", {trigger: true})
+		}})
+	}})
+}
